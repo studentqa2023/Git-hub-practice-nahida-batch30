481891a9759241d05f7e90de68681ca2417c5b64
studentqa2023 <121633696+studentqa2023@users.noreply.github.com>
2023-01-02 22:31:21 -0500

    Initial commit

---
+++ src/main/java/com/Genaric/HotelBooking.java
@@ -0,0 +1,17 @@
+package com.Genaric;
+
+
+public class HotelBooking {
+	
+	public void grtHotel() {
+		System.out.println("I want to book a hotel");		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		HotelBooking obj = new HotelBooking();	
+		obj.grtHotel();
+	}
+
+}
