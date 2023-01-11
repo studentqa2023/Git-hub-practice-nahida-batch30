45789a75d3fa841a473ef87c124c804445c16f1d
studentqa2023 <sarowerhome@gmail.com>
2023-01-10 22:13:15 -0500

    Added package com.practice & class Tiger

---
+++ src/main/java/com/practice/Tiger.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Tiger {
+	
+	public void getMoney() {
+		System.out.println("Tiger class");
+	}
+	
+	public static void main(String[] args) {
+		Tiger obj = new Tiger();
+		obj.getMoney();
+	}
+
+}
